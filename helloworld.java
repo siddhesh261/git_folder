8ca3d2fc8c8520b3d4bbf9a4e616ff600648b0c9
siddhesh261 <siddheshghode99@gmail.com>
2024-03-16 20:29:22 +0530

    change

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 public class helloworld {
     public static void main(String args[]){
-<<<<<<< HEAD
+
         System.out.println("Jenkins is awesome");
         System.out.println("Jenkins is awesome");
         System.out.println("Jenkins");
->>>>>>> e778e02f53507f0235c17a7a437f8ea012f0cddd
+
     }
 }
