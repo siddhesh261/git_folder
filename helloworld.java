de6e2f070d9d6d90f7fcfa0d82801b24a66121be
siddhesh261 <siddheshghode99@gmail.com>
2024-03-16 20:39:08 +0530

    YES

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld {
     public static void main(String args[]){
 
         System.out.println("Jenkins is awesome");
-        System.out.println("Jenkins is awesome");
+        System.out.println("Jenkins i awesome");
         System.out.println("Jekins");
 
     }
